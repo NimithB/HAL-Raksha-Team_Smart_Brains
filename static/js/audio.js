96f99a09d082c615080da5a8f31dbe784c6b8109
shrirampai3000 <shrirampai3000@gmail.com>
2025-02-02 07:27:02 +0530

    final

---
+++ static/js/audio.js
@@ -0,0 +1,4 @@
+function playButtonSound() {
+    const audio = new Audio('data:audio/wav;base64,UklGRnoGAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQoGAACBhYqFbF1NOTIyOUlbcIqfr7S0qZN5XjosJDJFYIGlxtnY0LSRZkEcBgAGJE2ElLa/vK6SYjcRAAAAAB1Lh7XW6OO5jVsvBwAAAAUxdavc//other_base64_data'); // Add actual audio data
+    audio.play();
+}
